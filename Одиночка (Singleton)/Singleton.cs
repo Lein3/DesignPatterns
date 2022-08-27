54bde54000da1011c2457bcdb7c1e47aac5fca57
Lein3 <semenshatilo@gmail.com>
2022-08-27 16:58:09 +0700

    Реализуем одиночка

---
+++ Одиночка (Singleton)/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Одиночка__Singleton_
+{
+    internal class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { 
+
+        }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+}
